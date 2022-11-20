855427764770bf85c34a3befc7d71594f33e9f78
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 15:56:17 +0300

    Create Two variables in the line.c

---
+++ Course from Udemy/Two variables in the line.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+  int balance;
+  balance=20;
+
+  int amount;
+  amount=150;
+
+printf("Your balance is now %d, and the amount of coins you need is %d", balance, amount);
+return 0;
+}
