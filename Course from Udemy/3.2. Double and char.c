1567af4ae83869cf4d36501ac569949b8898c095
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 16:26:50 +0300

    Create 3.2. Double and char.c

---
+++ Course from Udemy/3.2. Double and char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    double amount=1.3;
+    printf("The value of amount is %lf", amount);
+    char character='P';
+    printf("\nThe value of character is %c", character);
+return 0;
+}
