eab9f2d8aa5deb7efedc87a8cd2e38def3f31515
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-21 00:11:55 +0300

    Create 14. Relational operators.c

---
+++ Course from Udemy/14. Relational operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("Output of 4 == 5 relational operator is %d\n\n", 4 == 5);
+
+    int num1 = 10;
+    int num2 = 12;
+
+    printf("Here we work with %d and %d", num1, num2);
+    printf("\nThe output of %d <= %d is %d", num1, num2, num1 <= num2);
+    printf("\nThe output of num1 != num2 is %d\n\n", num1 != num2);
+
+return 0;
+}
