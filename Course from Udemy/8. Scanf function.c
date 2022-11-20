b9a2b524792e144076a2b40f3481a5eb94122f8e
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 22:12:04 +0300

    Create 8. Scanf function.c

---
+++ Course from Udemy/8. Scanf function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int count;
+    int amount;
+    scanf("%d%d", &count, &amount);
+    printf("The balance of count is %d, and the value of amount is %d.", count, amount);
+return 0;
+}
