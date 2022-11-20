83df832cdaada6b3c69f93f93ac41cb9c9404088
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 23:23:46 +0300

    Create 11. Multiplication of input numbers.c

---
+++ Course from Udemy/11. Multiplication of input numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int number1;
+    int number2;
+
+    printf("Enter number1: ");
+    scanf("%d", &number1);
+
+    printf("\nEnter number2: ");
+    scanf("%d", &number2);
+
+    printf("Multiplication of numbers %d and %d is %d\n\n", number1, number2, number1*number2);
+return 0;
+}
