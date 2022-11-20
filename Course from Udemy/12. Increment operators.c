fba326df7302df7fe86be7bec860c702f2bb6e18
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 23:46:23 +0300

    Create 12. Increment operators.c

---
+++ Course from Udemy/12. Increment operators.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int count;
+    printf("Print your count: ");
+    scanf("\n%d", &count);
+    printf("The increment of your count is %d\n\n.", ++count);
+return 0;
+}
