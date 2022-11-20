7caeddfe7251d30cb638bcd0217c50ee22280655
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 15:43:36 +0300

    Create (2) Variables assignment.c

---
+++ Course from Udemy/(2) Variables assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int _bullet;
+    _bullet = 101;
+    printf("This is me second assignment.\n");
+    printf("The value of variable is: %d", _bullet);
+return 0;
+}
+
+/* Ознакомился с типами данных, а также со спецификаторами для объявления переменных (которые и зависят от типа данных). */
