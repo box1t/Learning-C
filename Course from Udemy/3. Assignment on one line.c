f2c1a934e7238f4858dcde1d002ad2c4da69cb3b
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 16:09:35 +0300

    Create 3. Assignment on one line.c

---
+++ Course from Udemy/3. Assignment on one line.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int alfa=10;
+  float beta=10.5;
+  printf("The selling price was %d,\nbut it has grown up to %f", alfa, beta);
+return 0;
+}
+
+/* Пока что косячу с символами. Запомнил, что после 'beta' не нужны большие кавычки.
+Вводить текст по новой от руки выходит очень даже результативно, таким образом лучше усваивается материал. */
