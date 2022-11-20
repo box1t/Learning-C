854ca3c1a182d7c870faa39a3c72777cb51894fb
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 21:55:56 +0300

    Create 7. Sum of two numbers.c

---
+++ Course from Udemy/7. Sum of two numbers.c
@@ -0,0 +1,11 @@
+/* sum of two numbers */
+#include <stdio.h>
+
+int main(){
+    int sum = 0;
+    int operand1 = 10;
+    int operand2 = 21;
+    sum = operand1 + operand2;
+    printf("Sum = %d + %d = %d", operand1, operand2, sum);
+return 0;
+}
