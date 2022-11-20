6f104d05962bec325626f185d3c6b90ffb2b96af
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-20 22:00:22 +0300

    Added multiplication for 2 numbers.

--- Course from Udemy/7. Sum of two numbers.c
+++ Course from Udemy/7. Sum of two numbers.c
@@ -9,3 +9,19 @@ int main(){
     printf("Sum = %d + %d = %d", operand1, operand2, sum);
 return 0;
 }
+
+
+/* multiplication of two numbers */
+
+/* #include <stdio.h>
+
+int main(){
+    float result = 0;
+    float number1 = 2.0;
+    float number2 = 1.5;
+    result = number1 * number2;
+    printf("result = %f * %f = %f", number1, number2, result);
+return 0;
+} 
+
+*/
