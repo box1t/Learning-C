bcff1cd0ada43702dfddd2f0982a9417f7c6b2a8
prof. Preobrazhenskiy <113284506+box1t@users.noreply.github.com>
2022-11-26 22:13:07 +0300

    Create 2. Composite condition.c

---
+++ stepik/rep1. hard_basics/2. Composite condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+void in_range_3_7(int z) {
+   if (z > 3 && z < 7) { // z больше 3 и z меньше 7
+      printf("Yes"); 
+   } 
+   else { 
+      printf("No"); 
+   }
+}
+return 0;
+}
