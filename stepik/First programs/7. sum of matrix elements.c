d3f3bc7bd305ed8b3b65ea8687b29899d9206895
box1t <113284506+box1t@users.noreply.github.com>
2022-12-11 17:52:09 +0300

    Update 7. sum of matrix elements.c

--- stepik/First programs/7. sum of matrix elements.c
+++ stepik/First programs/7. sum of matrix elements.c
@@ -20,3 +20,44 @@ int main(){
     printf("%d", sum);
     return 0;
 }
+
+/* А теперь в виде функции:
+
+#include <stdio.h>
+
+
+#define N 3
+
+
+// сумма всех элементов массива
+int sum_all(int a[N][N])
+{
+    int sum = 0;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+             sum += a[i][j];
+        }
+    }
+    return sum;
+}
+
+
+int main()
+{
+    int a[N][N];
+
+
+    // чтение массива
+    for(int i = 0; i < N; i++) {
+        // чтение одной строки a[i]
+        for(int j = 0; j < N; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+
+    printf("%d\n", sum_all(a));
+    return 0;
+}
+
+*/
